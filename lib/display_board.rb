239a5b7a5484a70b1045d39ace401148b6d5a778
SULAIMAN - <sulaiman@ce.student.pens.ac.id>
2020-05-19 03:42:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,10 +7,8 @@ def display_board(board)
   puts "#{board[3] | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6] | #{board[7]} | #{board[8]} "
+end
+
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 display_board(board)
-O | X |
------------
-  | X |
------------
-X | O |
+
