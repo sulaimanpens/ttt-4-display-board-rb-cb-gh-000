87d7f935a97b85fc43e26c7c5acb5f895ca8e1f5
SULAIMAN - <sulaiman@ce.student.pens.ac.id>
2020-05-19 03:39:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  puts ""
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+display_board(board)
+O | X |   
+-----------
+  | X |   
+-----------
+X | O |   
